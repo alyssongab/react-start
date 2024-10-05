507280ecb9a9466390d1d52c5fb91647626b4a0b
Alysson Gabriel <alysson.gabriel61@gmail.com>
2024-10-04 22:55:42 -0400

    iniciando o roteamento de paginas

---
+++ todo/src/pages/TaskPage.jsx
@@ -0,0 +1,9 @@
+function TaskPage() {
+    return (
+        <div className="w-screen h-screen bg-zinc-900 flex justify-center p-6">
+            <p className="text-white">eae</p>
+        </div>
+    );
+}
+
+export default TaskPage;    
