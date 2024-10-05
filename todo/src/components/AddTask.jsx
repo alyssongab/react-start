0d88a2b875f97ebde30d62f4bbfe43819964f928
Alysson Gabriel <alysson.gabriel61@gmail.com>
2024-10-04 22:55:13 -0400

    criando a funcao handleAddTask dentro do componente AddTask

--- todo/src/components/AddTask.jsx
+++ todo/src/components/AddTask.jsx
@@ -5,19 +5,22 @@ function AddTask({onTaskAdd}) {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
 
-    function handleKeyDown(event) {
-        // Verifica se a tecla pressionada é "Enter"
-        if (event.key === "Enter") {
-            // Verifica se os campos não estão vazios
-            if (title.trim() && description.trim()) {
-                onTaskAdd(title, description);
-                // Limpa os campos após adicionar a tarefa
-                setTitle("");
-                setDescription("");
-            } else {
-                alert("You can't add empty tasks");
-            }
+    const handleKeyDown = (event) => {
+        if(event.key === "Enter") {
+            handleAddTask();
         }
+    }
+
+    const handleAddTask = () => {
+        if(!title.trim() || !description.trim()) {
+            return alert("Empty Fields!");
+        }
+            onTaskAdd(title, description);
+            
+            // Limpa os campos
+            setTitle("");
+            setDescription("");
+
     }
 
     return(
@@ -27,28 +30,22 @@ function AddTask({onTaskAdd}) {
             type="text" 
             placeholder="Type the task title"
             onChange={(event) => setTitle(event.target.value)}
+            value={title}
             onKeyDown={handleKeyDown}/>
 
             <input className="border border-slate-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-300 px-4 py-2 rounded-md" 
             type="text" 
             placeholder="Type the task description"
             onChange={(event) => setDescription(event.target.value)}
+            value={description}
             onKeyDown={handleKeyDown}/>
             
-           
-            <button onClick={() => {
-            
-            if(!title.trim() || !description.trim()) {
-                return alert("You can't add empty tasks")
-            }
-            onTaskAdd(title, description);
-            setTitle("");
-            setDescription("");
-            }}
+        
+            <button onClick={handleAddTask}
             className="w-3/4 mx-auto bg-indigo-600 p-2 rounded-md text-white hover:scale-105 transition-all" >
                 Add
             </button>
-
+        
         </div>
     );
 }
